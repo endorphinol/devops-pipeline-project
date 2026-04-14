8033470ea7afc30ef8e3d0c0866484b1cdb441de
Daniel K <89999804331@yandex.ru>
2026-04-15 01:35:16 +0300

    3. Обновлена структура файлов (Дополнен ci.yaml, обновлены манифесты k8s).

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "CI/CD"
+    return f"CI/CD"
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
